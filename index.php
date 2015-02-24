2c5e37c1fd03d565042c92918c5ab3b343ae0df6
Cadiducho <cadiducho@gmail.com>
2015-02-24 15:43:31 +0100

    Diseño básico de la nueva web

--- index.php
+++ index.php
@@ -1,11 +1,98 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        ?>
-    </body>
+<html lang="es">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Cadiducho.com | Inicio</title>
+
+  <!-- CSS  -->
+  <link href="datos/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="datos/css/font-awesome.min.css" rel="stylesheet">
+  <!-- <link href="datos/css/inicioWeb.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
+</head>
+<body>
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="container">
+      <div class="nav-wrapper"><a id="logo-container" href="#" class="brand-logo">Cadiducho.com</a>
+        <ul class="right">
+            <li><a href="http://Cadiducho.com/">Web</a></li>
+            <li><a href="http://blog.cadiducho.com/">Blog</a></li>
+            <li><a href="http://dev.cadiducho.com/">Dev</a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="side-nav">
+          <li><a href="http://Cadiducho.com/">Web</a></li>
+          <li><a href="http://blog.cadiducho.com/">Blog</a></li>
+            <li><a href="http://dev.cadiducho.com/">Dev</a></li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+      </div>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h1 class="header center orange-text">Cadiducho.com</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Rediseño de la web de inicio basado en Material Design</h5>
+      </div>
+    </div>
+	<div class="divider"></div>
+        <div class="section">
+            <div class="row center">
+                <h3 class="header">Redes Sociales</h3>
+                    <a href="https://twitter.com/Cadiducho" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    <a href="http://www.youtube.com/user/Cadiducho" class="btn btn-default btn-lg"><i class="fa fa-youtube fa-fw"></i> <span class="network-name">YouTube</span></a>
+                    <a href="https://www.github.com/Cadiducho" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    <a href="https://www.bitbucket.com/Cadiducho" class="btn btn-default btn-lg"><i class="fa fa-bitbucket fa-fw"></i> <span class="network-name">BitBucket</span></a>
+                    <a href="https://Telegram.me/Cadiducho" class="btn btn-default btn-lg"><i class="fa fa-paper-plane-o fa-fw"></i> <span class="network-name">Telegram</span></a>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="section">
+            <div class="row center">
+                <h3 class="header">Páginas destacadas</h3>
+                    <a href="http://blog.cadiducho.com/" class="btn btn-default btn-lg"><i class="fa fa-comments-o"></i> <span class="network-name">Blog</span></a>
+                    <a href="http://pass.cadiducho.com/" class="btn btn-default btn-lg"><i class="fa fa-check-square-o"></i> <span class="network-name">Generador de Contraseñas</span></a>
+                    <a href="http://dev.cadiducho.com/" class="btn btn-default btn-lg"><i class="fa fa-code-fork"></i> <span class="network-name">Dev (Proyectos)</span></a>
+            </div>
+        </div>
+  </div>
+
+  <footer class="page-footer orange">
+    <div class="container">
+      <div class="row">
+        <div class="col l7 s12">
+          <h5 class="white-text">Cadiducho.com</h5>
+          <p class="grey-text text-lighten-4">Web de proyectos realizados por <a class="orange-text text-lighten-3" href="https://twitter.com/Cadiducho">Cadiducho</a>. 
+		  </br>Toda la web está hecha sin animo de lucro. No contenemos anuncios ni vendemos tu información.
+		  </br>Al seguir navegando estás aceptando el uso de <a class="orange-text text-lighten-3" href="https://twitter.com/Cadiducho">Cookies</a> en nuestra web</p>
+
+
+        </div>
+        <div class="col offset-l3 s14">
+          <h5 class="white-text">Navegación</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Inicio</a></li>
+            <li><a class="white-text" href="#!">Acerca de</a></li>
+            <li><a class="white-text" href="#!">Contacto</a></li>
+            <li><a class="white-text" href="#!">Cookies</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      © 2015 Cadiducho
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+  <script src="datos/js/materialize.js"></script>
+  <!--<script src="datos/js/init.js"></script>-->
+
+  </body>
 </html>
